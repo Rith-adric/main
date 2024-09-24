8bdb1bf47b08cd0036468e7a2509dad15965b0f3
Rith Adric <160109430+Rith-adric@users.noreply.github.com>
2024-09-24 23:16:13 +0700

    my student dart

--- lib/pages/teachers.dart
+++ lib/pages/teachers.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Exams extends StatelessWidget {
-  const Exams({super.key});
+class Teachers extends StatelessWidget {
+  const Teachers({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +42,6 @@ class Exams extends StatelessWidget {
 
 void main() {
   runApp(const MaterialApp(
-    home: Exams(),
+    home: Teachers(),
   ));
 }
